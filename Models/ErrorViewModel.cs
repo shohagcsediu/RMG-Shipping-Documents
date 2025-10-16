6ade04b33292502b61c1cbe93922b6668fcbb269
Mohammad Shohag <shohagcsediu@gmail.com>
2025-10-16 11:04:34 +0600

    project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RMG_Shipping_Documents.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
